bcd9fa4bf3e6d7818a3faa7af1bfa0a74f36a36f
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-29 16:29:09 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
           <a
             href="https://github.com/shebadi/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
