fc09cf80a99e9ebdc89b93a11c41b1ffc18bcb11
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-29 16:02:33 +0430

    Remove unnecessary css code and js code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <a href="https://github.com/shebadi/react-weather-app"></a>
+    </div>
+  );
 }
