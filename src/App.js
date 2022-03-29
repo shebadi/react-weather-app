be64c9940f5cc4a1fdad2a92e79d1ce3d79938cd
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-29 16:06:32 +0430

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <a href="https://github.com/shebadi/react-weather-app"></a>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Shabnam Ebadi and is{" "}
+        <a href="https://github.com/shebadi/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
