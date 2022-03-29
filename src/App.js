0177e1db82b6d8fb710d731eb8b01dd2a101340b
ShabnamEbadi <sh.ebady@yahoo.com>
2022-03-29 16:18:55 +0430

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Shabnam Ebadi and is{" "}
-        <a href="https://github.com/shebadi/react-weather-app" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Shabnam Ebadi and is{" "}
+          <a
+            href="https://github.com/shebadi/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
