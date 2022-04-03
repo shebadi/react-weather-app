d3d1edf9242e8d0d01fefcd7ff756f1b186c2a57
ShabnamEbadi <sh.ebady@yahoo.com>
2022-04-03 15:55:26 +0430

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -26,7 +26,6 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 15%</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
